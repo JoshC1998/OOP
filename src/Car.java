14d3d141f104e30483c943c39863063dbe8557ff
Joshua Cruz <jcruz1998@gmail.com>
2024-11-04 10:21:32 -0500

    OOP

---
+++ src/Car.java
@@ -0,0 +1,13 @@
+public class Car {
+    String make = "Chevrolet";
+    String model = "Nissan";
+    int year = 2000;
+    double price = 50000.00;
+
+    void drive() {
+        System.out.println("You drive the car");
+    }
+    void brake(){
+        System.out.println("You step on the the brakes");
+    }
+}
